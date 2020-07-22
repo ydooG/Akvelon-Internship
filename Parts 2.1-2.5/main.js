9232752275673466d29acdf739591d915ea586f5
Rasim <rasim.2@bk.ru>
2020-07-22 20:21:35 +0300

    playing with varibales, datatypes

---
+++ Parts 2.1-2.5/main.js
@@ -0,0 +1,28 @@
+'use strict';
+
+alert('Hello!');
+
+let admin;
+let name;
+name = 'John';
+admin = name;
+alert(admin);
+
+let number = 123;
+number = 1.3;
+
+alert(number);
+alert(Infinity);
+alert(Infinity / "not a number"); // Answer Nan
+let str1 = 'quote';
+let srt2 = "double quote";
+let str3 = `backtick. a = ${1+2}`;
+alert(str3);
+let isAlive = true;
+let isTrue = 3 < 2;
+let age;
+alert(age); // returns undefined
+let nullik = null; // returns null
+typeof 9999999999999999999n; //bigInt
+
+
